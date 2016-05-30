a46bf88f7cc62abd2c9c8afa8eed176982721af9
pearsowill <pearsowill@users.noreply.github.com>
2016-05-30 18:52:05 +1200

    Variation of Quad 4
    Aims to only take on the cases that the current maze presents in Quad 4

--- Quadrant4.cpp
+++ Quadrant4.cpp
@@ -51,55 +51,24 @@ int main()
         printf("Left sensor: %d Middle sensor: %d Right sensor: %d\n", left_sensor, middle_sensor, right_sensor);
         printf("Pre Left: %d Pre mid: %d Pre right: %d\n", previous_left2, previous_middle2, previous_right2);
 
-
-        if (left_sensor < previous_left2-difference){//if left open
-            printf("Left\n");
-            wall_turn_left();
-            break;
-            continue;
-        }
-        else if (middle_sensor < 120 && right_sensor < previous_right2-difference){//if middle open and right open
-            printf("Straight\n");
-            set_motor(1,default_speed);
-            set_motor(2,3+default_speed);
-            while (true){
-                left_sensor = read_analog(0);
-                middle_sensor = read_analog(1);
-                right_sensor = read_analog(2);
-                if (left_sensor < previous_left2-difference){
-                    wall_turn_left();
-                    printf("left");
-                    break;
-                }
-                else if (middle_sensor > 496){
-                    break;
-                }
-                else if (right_sensor > previous_right2+difference){
-                    break;
-                }
-                previous_left = left_sensor;
-                previous_middle = middle_sensor;
-                previous_right = right_sensor;
-                previous_left1 = previous_left;
-                previous_middle1 = previous_middle;
-                previous_right1 = previous_right;
-                previous_left2 = previous_left1;
-                previous_middle2 = previous_middle1;
-                previous_right2 = previous_right1;
-            }
-            set_motor(1, 0);//right_motor); //right
-            set_motor(2, 0);
-            break;
-            continue;
-        }
-        else if (right_sensor < previous_right2-difference){
+        if (right_sensor < previous_right2-difference){
             printf("Right\n");
             wall_turn_right();
+            difference += 150;
             break;
             continue;
         }
-        else if (middle_sensor < 496){//if wall either side, do PID stuff
+        
+        else if (left_sensor < previous_left2-difference){//if left open
+            printf("Left\n");
+            wall_turn_left();
+            difference += 150;
+            break;
+            continue;
+        }
+        else if (middle_sensor < 400){//if wall either side, do PID stuff
             printf("Yay\n");
+            difference = 40;
             current_error = right_sensor - left_sensor;
             proportional_signal = current_error*kp;
 
@@ -121,14 +90,6 @@ int main()
             previous_right2 = previous_right1;
 
         }
-        else (left_sensor < right_sensor + 100 || left_sensor > right_sensor - 100)
-        {
-            printf("Dead end\n");
-            wall_dead_end();
-            break;
-        }
-        
-
     }
 }
 
